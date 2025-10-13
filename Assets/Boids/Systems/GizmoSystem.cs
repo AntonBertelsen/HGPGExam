cf402687182bfb0d39682b4f5bf7e0f22cf0d2b1
JonasUJ <jj@ifs.aero>
2025-10-13 21:31:51 +0200

    Add GizmoSystem

---
+++ Assets/Boids/Systems/GizmoSystem.cs
@@ -0,0 +1,35 @@
+using System.Linq;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+public partial struct GizmoSystem : ISystem
+{
+    private EntityQuery _boidQuery;
+
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+        _boidQuery = new EntityQueryBuilder(Allocator.Temp)
+            .WithAll<BoidTag, LocalTransform, Velocity>()
+            .Build(ref state);
+    }
+
+    public void DrawGizmos()
+    {
+        DrawBoidGizmos();
+    }
+
+    private void DrawBoidGizmos()
+    {
+        var transforms = _boidQuery.ToComponentDataArray<LocalTransform>(Allocator.TempJob);
+        var velocities = _boidQuery.ToComponentDataArray<Velocity>(Allocator.TempJob);
+
+        foreach (var (transform, velocity) in transforms.Zip(velocities, (t, v) => (t, v)))
+        {
+            Gizmos.DrawRay(transform.Position, Vector3.Normalize(velocity.Value));
+        }
+    }
+}
